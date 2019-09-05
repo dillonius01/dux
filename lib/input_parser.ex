52562ab7a5bd07635f1dcf2ca07e7049b995a371
Dillon Powers <dillon.c.powers@gmail.com>
2019-09-05 15:26:29 -0400

    adds parsing of duck commands

--- lib/input_parser.ex
+++ lib/input_parser.ex
@@ -1,34 +1,36 @@
 defmodule Dux.InputParser do
   alias Dux.Duck
 
-  @valid_commands [:starboard, :port]
-
   def parse_duck_position(raw_input) when is_binary(raw_input) do
     [x, y] =
       raw_input
-      |> String.downcase()
-      |> String.trim()
-      |> String.split(" ")
+      |> clean_and_split(" ")
       |> Enum.map(&String.to_integer(&1))
 
     {:ok, %{x: x, y: y}}
   end
 
-  def parse_duck_commands(raw_input, duck \\ %Duck{}) when is_binary(raw_input) do
+  def parse_duck_commands(raw_input) when is_binary(raw_input) do
     commands =
       raw_input
-      |> String.downcase()
-      |> String.trim()
-      |> String.split(" ")
+      |> clean_and_split("", trim: true)
       |> Enum.map(&convert_to_command/1)
 
     {:ok, commands}
+  end
+
+  defp clean_and_split(raw_input, split_on, split_options \\ []) do
+    raw_input
+    |> String.downcase()
+    |> String.trim()
+    |> String.split(split_on, split_options)
   end
 
   defp convert_to_command(string_command) do
     case string_command do
       "s" -> :starboard
       "p" -> :port
+      "f" -> :forward
     end
   end
 end
--- test/input_parser_test.exs
+++ test/input_parser_test.exs
@@ -3,19 +3,33 @@ defmodule Dux.InputParserTest do
   alias Dux.InputParser
   alias Dux.Duck
 
-  test "can parse space-separated input" do
-    raw_input = "5 5"
+  describe "parse_duck_position/1" do
+    test "can parse space-separated input" do
+      raw_input = "5 5"
 
-    actual = InputParser.parse_duck_position(raw_input)
+      actual = InputParser.parse_duck_position(raw_input)
 
-    assert match?({:ok, %{x: 5, y: 5}}, actual)
+      assert match?({:ok, %{x: 5, y: 5}}, actual)
+    end
+
+    # test "returns error tuple if bad input" do
+    #   raw_input = "five five"
+
+    #   actual = InputParser.parse_duck_position(raw_input)
+
+    #   assert match?({:error, _}, actual)
+    # end
   end
 
-  test "returns error tuple if bad input" do
-    raw_input = "five five"
+  describe "parse_duck_commands/1" do
+    test "can parse non-separated list of directions" do
+      raw_input = "PFPFS"
 
-    # actual = InputParser.parse_duck_position(raw_input)
+      actual = InputParser.parse_duck_commands(raw_input)
 
-    # assert match?({:error, _}, actual)
+      expected = [:port, :forward, :port, :forward, :starboard]
+
+      assert match?({:ok, [:port, :forward, :port, :forward, :starboard]}, actual)
+    end
   end
 end
